69863e34abf42094fd8978b4a95c33e2eb4e5248
Charles Ge <char1209ge@gmail.com>
2018-11-14 12:22:19 -0800

    Reformat code smell

--- analysis_tool_python/crawl_etherscan.py
+++ analysis_tool_python/crawl_etherscan.py
@@ -4,20 +4,22 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 Soup = BeautifulSoup
 
-def get_etherscan_block_page(block_hash, raw_url):
-    # two urls are both ok
-    # /block/block_height or /block/block_hash
-    # url = 'https://etherscan.io/block/{}'.format(block_hash)
-    url = raw_url.format(block_hash)
-    page = requests.get(url)
-    # print(page.content)
-    return page
 
-def get_txs_list_page(url):
-    # url example: https://www.etherchain.org/block/6429184
+url_etherscan_pre = 'https://etherscan.io/block/'
+url_etherchain_pre = 'https://etherchain.org/block/'
+
+
+def get_block_page(block_number):
+    # /block/block_height or /block/block_hash are both ok
+    url = url_etherscan_pre + str(block_number)
+    return requests.get(url)
+
+
+def get_txs_list_page(block_number):
     # use this website because it contains all txs in one page
-    page = requests.get(url)
-    return page.content
+    url = url_etherchain_pre + str(block_number)
+    return requests.get(url).content
+
 
 def parse_txs_list_page_content(page_content):
     # parse the page and return all txs in list
@@ -85,71 +87,33 @@ def parse_txs_list_page_content(page_content):
 
 
 def parse_block_page_content(page_content, block_number):
-
-    table_dict = {}
+    table_dict = dict()
     soup = BeautifulSoup(page_content, 'html.parser')
     table_content = soup.find(id="ContentPlaceHolder1_maintable")
     info_list = table_content.find_all('div')
-    # print(info_list)
-    # print('\n')
     # iteration the table content in html
     i = 0
     while i < len(info_list):
-        # print(i)
         raw_key = info_list[i]
-        key = raw_key.contents[0].split(':')[0]
-        # print("key:")
-        # print(key)
         i += 1
-        # print(i)
         raw_value = info_list[i]
-        value = raw_value.contents[0]
-        # print("value:")
-        # print(value)
-        # print('\n')
-
-        table_dict[key] = value
         i += 1
+        key = raw_key.contents[0].split(':')[0]
+        value = raw_value.contents[0]
+        table_dict[key] = value
 
-    # get Parent Hash value from the <a href=xxx>parent_hash_value</a>
-    raw_parent_hash = table_dict['Parent Hash']
-    table_dict['Parent Hash'] = raw_parent_hash.contents[0]
+    table_dict['Parent Hash'] = table_dict['Parent Hash'].contents[0]
+    table_dict['Mined By'] = table_dict['Mined By'].contents[0]
+    table_dict['Height'] = str(block_number)
 
-    # get the minor hash value from <a>minor_hash_value</a>
-    raw_minor_hash = table_dict['Mined By']
-    table_dict['Mined By'] = raw_minor_hash.contents[0]
-
-    # process the txs link and get the returned txs list
-    # raw_txs_href = table_dict['Transactions']
-    block_height_split_from_txs_href = str(block_number)
-    # original key, value in the dict contains the html format symbals
-    # example: 'Height': '\n\xa06429184\xa0\xa0\n'
-    # so, use the value from txs
-    table_dict['Height'] = block_height_split_from_txs_href
-
-    # ==============process txs==========================
+    # process txs
     # use etherchain instead of etherscan because chain contains all info in one page
-    # https: // www.etherchain.org / block / 6429184
-    txs_list_page_link = 'https://etherchain.org/block/' + block_height_split_from_txs_href
-    tx_list = parse_txs_list_page_content(get_txs_list_page(txs_list_page_link))
-    # print(txs_list_page_link)
+    tx_list = parse_txs_list_page_content(get_txs_list_page(block_number))
     table_dict['Transactions'] = tx_list
-    # ===================================================
 
     table_dict['received status'] = 'inserted'
-    # print(table_dict)
     return table_dict
 
-def dump_to_json(table_dict):
-    pass
-
-def process_a_block(block_hash):
-    # outer funcs should call this func to process a block
-    # return the block info in json
-    page_content = get_etherscan_block_page(block_hash, 'https://etherscan.io/block/{}')
-    table_dict = parse_block_page_content(page_content)
-    json_data = json.dumps(table_dict)
-    return json_data
 
 def json_to_string(json_data):
     rs = '[2018] [Inserted]'
@@ -163,26 +127,25 @@ def json_to_string(json_data):
     rs += '\n'
     return rs
 
+
 def iteration(block_number, saved_file):
     started_at = datetime.now()
     i = 0
     while True:
-        try:
-            print(f"loading block {block_number}")
-            page = get_etherscan_block_page(str(block_number), 'https://etherscan.io/block/{}')
-            if 'There are no matching entries' in page.text:
-                break
-            with open(f"{saved_file}{datetime.now().strftime('%Y-%m-%d')}.txt", 'a') as f:
-                f.write(json_to_string(parse_block_page_content(page.content, block_number)))
-            block_number += 1
-            i += 1
-            print(f"Average time for {i} blocks: {((datetime.now()-started_at).seconds) / i} seconds")
-        except:
-            print(f"Exception. Retrying...")
+        print('loading block ' + str(block_number))
+        page = get_block_page(block_number)
+        if 'There are no matching entries' in page.text:
+            break
+        tx = parse_block_page_content(page.content, block_number)
+        time = tx['TimeStamp'].split('(')[1].split(')')[0]
+        filename = datetime.strptime(time, '%b-%d-%Y %I:%M:%S %p +%Z').replace(tzinfo=None).strftime('%Y-%m-%d')
+        with open(saved_file + filename + '.txt', 'a') as f:
+            f.write(json_to_string(tx))
+        block_number += 1
+        i += 1
+        print(f"Average time for {i} blocks: {((datetime.now()-started_at).seconds) / i} seconds")
 
 
 if __name__ == '__main__':
-    # iteration(6546781, '../records/blocks/canonical/') AWS saved state
-    iteration(6606556, '../records/blocks/canonical/')  # Ali cloud
-
-# Exception tx: 6355792, 6355801, 6355815
+    # iteration(6549581, '../records/blocks/canonical/') AWS
+    iteration(6614825, '../records/blocks/canonical/')  # Ali cloud
