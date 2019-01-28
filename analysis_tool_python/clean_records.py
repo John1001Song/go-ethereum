c4c73f40819a687a0049b6a5283eeb790157b998
CharlesGe129 <char1209ge@gmail.com>
2019-01-27 18:11:14 -0800

    If date format failed, use the date of last line.

--- analysis_tool_python/clean_records.py
+++ analysis_tool_python/clean_records.py
@@ -10,8 +10,8 @@ def clean_tx(file_path):
     u = dict()
     rs = ''
     date_ok = True
-    if not file_path.split('/')[-1].startswith('2018'):
-        return
+    filename = file_path.split('/')[-1]
+    last_date = f'[{filename.split("_")[0]} {filename.split("_")[1].split(".")[0]}:00:00.015303602 +0000 UTC m=+196751.022868978]'
     with open(file_path, 'r') as f:
         print(f"Cleaning {file_path}")
         while True:
@@ -20,10 +20,10 @@ def clean_tx(file_path):
                 break
             if '0x' not in line:
                 continue
-            line = clean_date(line)
             h = line.split("Hash=")[1].split(", ")[0] if "Hash=" in line else line.split(' ')[-1].strip('\n')
             if h not in u:
-                date_ok = date_ok & try_parse_date(line)
+                this_date_ok, line, last_date = try_parse_date(line, last_date)
+                date_ok = date_ok & this_date_ok
                 u[h] = 1
                 rs += line
             else:
@@ -35,38 +35,36 @@ def clean_tx(file_path):
     backup(file_path, f"{file_path.split('/txs/')[0]}/txs/backup/{file_path.split('/txs/')[1]}")
 
 
-def try_parse_date(line):
+def try_parse_date(line, last_date):
     created_at = line.split(" m=")[0].strip('[')
     time = created_at.split('.')[0] + ' ' + ' '.join(created_at.split(' ')[2:])
     try:
         datetime.strptime(time, '%Y-%m-%d %H:%M:%S %z %Z').replace(tzinfo=None)
-        return True
+        last_date = line.split(' Hash=')[0]
+        return True, line, last_date
     except:
-        print(f"Date format is wrong at: {line}")
-        return False
-
-
-def clean_date(line):
-    if line.count('[2') == 2:
-        line = '[2018' + line.split('[2018')[-1]
-    if line.startswith('8-'):
-        line = '[201' + line
-    if line.startswith('18-'):
-        line = '[20' + line
-    if line.startswith('018-'):
-        line = '[2' + line
-    return line
+        try:
+            # print(f"Try date format at: {line}")
+            line = last_date + " Hash=" + line.split(" Hash=")[1]
+            # print(f"New line: {line}")
+            created_at = line.split(" m=")[0].strip('[')
+            time = created_at.split('.')[0] + ' ' + ' '.join(created_at.split(' ')[2:])
+            datetime.strptime(time, '%Y-%m-%d %H:%M:%S %z %Z').replace(tzinfo=None)
+            return True, line, last_date
+        except:
+            print(f"Date format goes wrong at: {line}")
+            return False, line, last_date
 
 
 def clean_txs():
     for f in os.listdir(RECORD_PATH + '/txs'):
-        if not f.startswith('2018'):
+        if not f.endswith('.txt'):
             continue
         try:
             file_path = RECORD_PATH + '/txs/' + f
             clean_tx(file_path)
-        except:
-            print(f"file {f} went wrong")
+        except Exception as e:
+            print(f"file {f} went wrong: {e}")
 
 
 if __name__ == '__main__':
